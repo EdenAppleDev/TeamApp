1e5a97a2318fa6c47e8c760ba44beb27505765b8
eden <noedenkim@gmail.com>
2025-05-22 17:30:30 +0900

    change image name to English

--- TeamApp/ViewController.swift
+++ TeamApp/ViewController.swift
@@ -30,6 +30,7 @@ class ViewController: UIViewController {
     
     struct TeamMember {
         var mbti: String
+        var imageName: String
         var name: String
         var explanation: String
         var pros: String
@@ -37,11 +38,11 @@ class ViewController: UIViewController {
         var blog: String
     }
     
-    let myeonggyun = TeamMember(mbti: "ENFP", name: "송명균", explanation: "앱 개발에 흥미를 느끼며, 집중한 모습", pros: "소통을 하며, 가장 좋은 방향으로 간다.", style: "포옹력이 높으며, 주변을 많이 두리번 거림", blog: "gksmf0160@tistory.com")
+    let myeonggyun = TeamMember(mbti: "ENFP", imageName: "song", name: "송명균", explanation: "앱 개발에 흥미를 느끼며, 집중한 모습", pros: "소통을 하며, 가장 좋은 방향으로 간다.", style: "포옹력이 높으며, 주변을 많이 두리번 거림", blog: "gksmf0160@tistory.com")
     
-    let eden = TeamMember(mbti: "INTJ", name: "김이든", explanation: "비전공자지만 개발에 흥미를 느껴 더 성장하고 싶다는 마음으로 iOS 개발에 도전하게 됐습니다. MBTI가 I라 낯을 많이 가리는 편이지만,\n먼저 다가와주는 사람과는 편하게 잘 어울립니다.\n팀 프로젝트에서는 책임감을 가지고 적극적으로 참여하고, 열정 있게 함께하는 사람들과 협업하는 걸 좋아합니다.", pros: "포기하지 않고 끝까지 해결하려는 끈기와 문제 해결에 대한 흥미를 바탕으로, 어려운 상황에서도 스스로 답을 찾아가는 힘이 있습니다.", style: "무언가를 빠르게 완성하기보다는, 제대로 이해하고 정리된 결과물을 만들어내는 것을 더 중요하게 생각합니다.\n과정 하나하나를 꼼꼼하고 세심하게 접근하는 성격입니다.", blog: "https://velog.io/@edenkim/posts")
+    let eden = TeamMember(mbti: "INTJ", imageName: "kim", name: "김이든", explanation: "비전공자지만 개발에 흥미를 느껴 더 성장하고 싶다는 마음으로 iOS 개발에 도전하게 됐습니다. MBTI가 I라 낯을 많이 가리는 편이지만,\n먼저 다가와주는 사람과는 편하게 잘 어울립니다.\n팀 프로젝트에서는 책임감을 가지고 적극적으로 참여하고, 열정 있게 함께하는 사람들과 협업하는 걸 좋아합니다.", pros: "포기하지 않고 끝까지 해결하려는 끈기와 문제 해결에 대한 흥미를 바탕으로, 어려운 상황에서도 스스로 답을 찾아가는 힘이 있습니다.", style: "무언가를 빠르게 완성하기보다는, 제대로 이해하고 정리된 결과물을 만들어내는 것을 더 중요하게 생각합니다.\n과정 하나하나를 꼼꼼하고 세심하게 접근하는 성격입니다.", blog: "https://velog.io/@edenkim/posts")
     
-    let taekyoung = TeamMember(mbti: "ENTP", name: "이태경", explanation: "iOS에 내 어플을 개발해서 등록하기 위해 개발을 배우고 있습니다.", pros: "단점이 없는것이 장점이다.", style: "변화가 내 고유의 스타일이다.", blog: "https://www.notion.so/312fe82fdbe94785a3fa76027ca62a3e")
+    let taekyoung = TeamMember(mbti: "ENTP", imageName: "lee", name: "이태경", explanation: "iOS에 내 어플을 개발해서 등록하기 위해 개발을 배우고 있습니다.", pros: "단점이 없는것이 장점이다.", style: "변화가 내 고유의 스타일이다.", blog: "https://www.notion.so/312fe82fdbe94785a3fa76027ca62a3e")
     
     
     override func viewDidLoad() {
@@ -93,7 +94,7 @@ class ViewController: UIViewController {
     }
     
     func changeMemberView(_ changeName: ViewController.TeamMember){
-        memberImageView.image = UIImage(named: changeName.name)
+        memberImageView.image = UIImage(named: changeName.imageName)
         mbtiLabel.text = changeName.mbti
         nameLabel.text = changeName.name
         explanationLabel.text = changeName.explanation
